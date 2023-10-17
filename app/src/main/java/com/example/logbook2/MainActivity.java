2622d0cc58fa343ac74114d034af9ac4c44ca406
MinhTri24 <tripmgcd20163@fpt.edu.vn>
2023-10-17 12:49:23 +0700

    Update Project

--- app/src/main/java/com/example/logbook2/MainActivity.java
+++ app/src/main/java/com/example/logbook2/MainActivity.java
@@ -8,34 +8,34 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    Button previousButton, nextButton;
-    int[] imageIds = { R.drawable.img1, R.drawable.img2, R.drawable.img3 };
-    int currentImageIndex = 0;
+    ImageView imgView;
+    Button preBtn, nextBtn;
+    int[] images = { R.drawable.img1, R.drawable.img2, R.drawable.img3 };
+    int currentImage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-        previousButton = findViewById(R.id.prevButton);
-        nextButton = findViewById(R.id.nextButton);
+        imgView = findViewById(R.id.imageView);
+        preBtn = findViewById(R.id.prevButton);
+        nextBtn = findViewById(R.id.nextButton);
 
-        previousButton.setOnClickListener(v -> {
-            currentImageIndex--;
-            if (currentImageIndex < 0) {
-                currentImageIndex = imageIds.length - 1;
+        preBtn.setOnClickListener(v -> {
+            currentImage--;
+            if (currentImage < 0) {
+                currentImage = images.length - 1;
             }
-            imageView.setImageResource(imageIds[currentImageIndex]);
+            imgView.setImageResource(images[currentImage]);
         });
 
-        nextButton.setOnClickListener(v -> {
-            currentImageIndex++;
-            if (currentImageIndex >= imageIds.length) {
-                currentImageIndex = 0;
+        nextBtn.setOnClickListener(v -> {
+            currentImage++;
+            if (currentImage >= images.length) {
+                currentImage = 0;
             }
-            imageView.setImageResource(imageIds[currentImageIndex]);
+            imgView.setImageResource(images[currentImage]);
         });
 
     }
